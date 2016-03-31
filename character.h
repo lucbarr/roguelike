39ed0d9009cbe72a3ce385cca2cae0802ac6cf8f
Luciano Barreira <meckmao@gmail.com>
2016-03-30 21:23:20 -0300

    Added character class

---
+++ character.h
@@ -0,0 +1,50 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "enum.h"
+
+class Character {
+private:
+	int hitPoints_;
+	int attack_;
+	bool isDead_;
+	int x_, y_;
+public:
+	Character(int hitPoints, int attack) :
+			hitPoints_(hitPoints),
+			attack_(attack),
+			isDead_(false),
+			x_(0),
+			y_(0)
+	{}
+	
+	int getHitPoints() const { return hitPoints_; }
+	int getAttack() const { return attack_; }
+	int getX() const { return x_; }
+	int getY() const { return y_; }
+
+	void changeHitPoints (int val) {
+		hitPoints_ += val;
+		if (hitPoints_ <= 0)
+			isDead_ = true;
+	}
+
+	void move(Direction dir) {
+		switch (dir) {
+			case NORTH:
+				y_--;
+				break;
+			case SOUTH:
+				y_++;
+				break;
+			case WEST:
+				x_--;
+				break;
+			case EAST:
+				x_++;
+				break;
+		}
+	}
+};
+
+#endif
---
+++ enum.h
@@ -0,0 +1,11 @@
+#ifndef ENUM_H
+#define ENUM_H
+
+enum Direction {
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST
+};
+
+#endif
